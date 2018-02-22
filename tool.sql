c617c844410a36463dc7349e676bfabc00b0c5bb
Vikaras <33661628+Vikaras@users.noreply.github.com>
2018-02-22 15:04:45 +0200

    Start of a new project

---
+++ tool.sql
@@ -0,0 +1,274 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2018 at 01:13 PM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tool`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `inv_id` int(11) NOT NULL,
+  `invcode` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `manufactid` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory_perm`
+--
+
+CREATE TABLE `inventory_perm` (
+  `inv_id` int(11) NOT NULL,
+  `invcode` tinyint(1) NOT NULL,
+  `description` tinyint(1) NOT NULL,
+  `location` tinyint(1) NOT NULL,
+  `value` tinyint(1) NOT NULL,
+  `comment` tinyint(1) NOT NULL,
+  `manufactid` tinyint(1) NOT NULL,
+  `edit` tinyint(1) NOT NULL,
+  `filter` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_filter`
+--
+
+CREATE TABLE `job_filter` (
+  `job_id` int(11) NOT NULL,
+  `job_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `job_filter`
+--
+
+INSERT INTO `job_filter` (`job_id`, `job_name`) VALUES
+(1, 'Employee'),
+(2, 'Customer'),
+(3, 'Supplier'),
+(4, 'Partner');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL,
+  `lists` varchar(255) NOT NULL,
+  `table_id` int(11) NOT NULL,
+  `comments` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `inv_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tables`
+--
+
+CREATE TABLE `tables` (
+  `table_id` int(11) NOT NULL,
+  `table_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tables`
+--
+
+INSERT INTO `tables` (`table_id`, `table_name`) VALUES
+(1, 'Users'),
+(2, 'Inventory');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `dob` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `job` varchar(255) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `job_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_perm`
+--
+
+CREATE TABLE `users_perm` (
+  `user_id` int(11) NOT NULL,
+  `name` tinyint(1) NOT NULL,
+  `lastname` tinyint(1) NOT NULL,
+  `dob` tinyint(1) NOT NULL,
+  `phone` tinyint(1) NOT NULL,
+  `job` tinyint(1) NOT NULL,
+  `comment` tinyint(1) NOT NULL,
+  `edit` tinyint(1) NOT NULL,
+  `filter` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`inv_id`),
+  ADD KEY `inv_id` (`inv_id`);
+
+--
+-- Indexes for table `inventory_perm`
+--
+ALTER TABLE `inventory_perm`
+  ADD PRIMARY KEY (`inv_id`),
+  ADD KEY `inv_id` (`inv_id`);
+
+--
+-- Indexes for table `job_filter`
+--
+ALTER TABLE `job_filter`
+  ADD PRIMARY KEY (`job_id`),
+  ADD KEY `job_id` (`job_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `inv_id` (`inv_id`),
+  ADD KEY `table_name` (`table_id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `tables`
+--
+ALTER TABLE `tables`
+  ADD PRIMARY KEY (`table_id`),
+  ADD KEY `table_id` (`table_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `job_id` (`job_id`);
+
+--
+-- Indexes for table `users_perm`
+--
+ALTER TABLE `users_perm`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `inventory_perm`
+--
+ALTER TABLE `inventory_perm`
+  MODIFY `inv_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `job_filter`
+--
+ALTER TABLE `job_filter`
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tables`
+--
+ALTER TABLE `tables`
+  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users_perm`
+--
+ALTER TABLE `users_perm`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`inv_id`) REFERENCES `inventory_perm` (`inv_id`),
+  ADD CONSTRAINT `inventory_ibfk_2` FOREIGN KEY (`inv_id`) REFERENCES `reports` (`inv_id`);
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`table_id`) REFERENCES `tables` (`table_id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_perm` (`user_id`),
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `reports` (`user_id`),
+  ADD CONSTRAINT `users_ibfk_3` FOREIGN KEY (`job_id`) REFERENCES `job_filter` (`job_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
