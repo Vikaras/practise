38e5575cbf5ca543afd9ff8148127d0eff730df4
Vikaras <arturasch@gmail.com>
2018-02-22 15:16:20 +0200

    Adding JS, PHP folders and gitignore.

---
+++ php/db.php
@@ -0,0 +1,16 @@
+<?php
+$server = "localhost";
+$user = "root";
+$db = "test";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$server; dbname=$db", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
