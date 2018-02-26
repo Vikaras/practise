a5925efbed36060fc48e4e730d12f6e2ee250bb1
Vikaras <arturasch@gmail.com>
2018-02-26 14:45:47 +0200

    Adding tbl_config.php file

---
+++ php/tbl_config.php
@@ -0,0 +1,43 @@
+<?php
+
+include "db.php";
+
+if(isset($_POST['checkboxes'])) {
+    $arr = json_decode($_POST['checkboxes']);
+    $tbl = $_POST['tbl'];
+    $repName = $_POST['repName'];
+    $repComments = $_POST['repComments'];
+
+    $stmt = $conn->prepare("SELECT * FROM tables WHERE table_id = " . $tbl . " LIMIT 1");
+
+    $stmt->execute();
+    $row = $stmt->fetch();
+
+    $columns_table = strtolower($row['table_name']) . '_cols';
+
+    $colValues = [];
+    foreach ($arr as $value) {
+        $colValues[$value->name] = $value->value;
+    }
+
+    $sql = "INSERT INTO reports (lists, table_id, comments)
+    VALUES('" . $repName . "', " . $tbl . ", '" . $repComments . "')";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->execute();
+
+    $report_id = $conn->lastInsertId();
+
+
+    $sql2 = "INSERT INTO $columns_table (report_id, " . implode(", ", array_keys($colValues)). ")
+    VALUES(" . $report_id . ", " . implode(", ", array_values($colValues)) . ")";
+
+
+    $stmt = $conn->prepare($sql2);
+
+        $stmt->execute();
+}
+
+?>
+
